a6d1400abfb6b41262978888719023c454df4e60
Starkov Ivan <cambogiyt@gmail.com>
2024-09-12 00:48:29 +0300

    project done

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Piskanaft/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,26 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+function createCard(card, onСlick) {
+  const cardTemplate = document.querySelector("#card-template").content.cloneNode(true);
+  const cardElement = cardTemplate.querySelector(".card");
+  cardElement.querySelector(".card__image").src = card.link;
+  cardElement.querySelector(".card__title").textContent = card.name;
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+  deleteButton.addEventListener("click", onСlick);
+  return cardElement;
+}
+
+function removeCard(event) {
+  const card = event.target.parentElement;
+  card.remove();
+}
+
+function addCard(card) {
+  const cardElement = createCard(card, removeCard);
+  const cardsList = document.querySelector(".places__list");
+  cardsList.append(cardElement);
+}
+
+initialCards.forEach(card => addCard(card));
